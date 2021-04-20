a1340b69c2f56f533af1523b7060e4ed3e3c59a9
ejkim <biz.ejkim@gmail.com>
2021-04-20 22:41:11 +0900

    크롭기능 추가

--- app/src/main/java/com/example/mygallery/MainActivity.kt
+++ app/src/main/java/com/example/mygallery/MainActivity.kt
@@ -27,11 +27,39 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(Intent.ACTION_PICK)
         intent.type = "image/*"
         startActivityForResult(intent, REQUEST_GALLERY_TAKE)
+
+        // 구글 갤러리 접근
+//        intent.type = "image/*"
+//        intent.action = Intent.ACTION_GET_CONTENT
+//        startActivityForResult(intent, PICK_IMAGE)
+
+        // 일반 갤러리 접근
+/*        intent.type = android.provider.MediaStore.Images.Media.CONTENT_TYPE
+        startActivityForResult(intent, PICK_IMAGE)*/
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         val imageView = findViewById<ImageView>(R.id.image)
+
+        // Check which request we're responding to
+        /*if (requestCode == PICK_IMAGE) {
+            // Make sure the request was successful
+            if (resultCode == RESULT_OK) {
+                try {
+                    // 선택한 이미지에서 비트맵 생성
+                    val inputStream =
+                        contentResolver.openInputStream(data?.data!!)
+                    val img = BitmapFactory.decodeStream(inputStream);
+                    inputStream?.close();
+                    // 이미지뷰에 세팅
+                    imageView.setImageBitmap(img);
+
+                } catch (e: Exception) {
+                    e.printStackTrace();
+                }
+            }
+        }*/
 
         when(requestCode){
             1 -> {
